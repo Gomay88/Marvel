6c0cf7f32aba6ee69e451080e8e59afff620428f
Víctor Jiménez <gomi88@hotmail.es>
2020-05-08 19:56:56 +0200

    Added links to character detail

--- Marvel/Presentation/CharacterDetail/CharacterDetailViewController.swift
+++ Marvel/Presentation/CharacterDetail/CharacterDetailViewController.swift
@@ -37,6 +37,29 @@ class CharacterDetailViewController: UIViewController, Spinnable {
                 self.comicsAvailable.text = "Comics: " + String(character.comics.available)
                 self.seriesAvailable.text = "Series: " + String(character.series.available)
                 self.storiesAvailable.text = "Stories: " + String(character.stories.available)
+                
+                for urls in character.urls {
+                    let paragraphStyle = NSMutableParagraphStyle()
+                    paragraphStyle.alignment = .left
+                    
+                    let font = UIFont.systemFont(ofSize: 16)
+                    
+                    let attributes: [NSAttributedString.Key: Any] = [
+                        .paragraphStyle: paragraphStyle,
+                        .link: urls.url,
+                        .font: font
+                    ]
+                    
+                    let attributedString = NSMutableAttributedString(string: urls.type.capitalized, attributes: attributes)
+                    
+                    let urlView = UITextView()
+                    urlView.attributedText = attributedString
+                    urlView.sizeToFit()
+                    urlView.isScrollEnabled = false
+                    urlView.translatesAutoresizingMaskIntoConstraints = true
+                    
+                    self.stackView.addArrangedSubview(urlView)
+                }
             })
         }
     }
